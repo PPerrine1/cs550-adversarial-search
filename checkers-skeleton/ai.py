a98bfcfd82a1fb7e9fe029c2fbbd57515f31f64c
nmillere <nmillere@gmail.com>
2020-03-11 17:54:00 -0700

    Messing around a bit

--- checkers-skeleton/ai.py
+++ checkers-skeleton/ai.py
@@ -10,15 +10,12 @@ class Strategy(abstractstrategy.Strategy):
 
     def play(self, board):
         # TODO
-        raise NotImplemented
-
-    def utility(self):
-        # TODO: Subclass of Strategy
 
         raise NotImplemented
-    
-    def AlphaBetaMinimax():
-        # TODO: Alpha-beta pruning minimax search, separate function or class
+
+    def utility(self, state):
+        # TODO
+
         raise NotImplemented
 
     def alpha_beta_search(self, state):
@@ -26,6 +23,15 @@ class Strategy(abstractstrategy.Strategy):
         """
             v = max_val(state, alpha=-infinity, beta=+infinity)
             return action in actions(state) with value v
+        """
+        # v = self.max_val(state, alpha=-float("inf"), beta=float("inf"))
+        #
+        # return [action for action in state.get_actions(self.maxplayer)
+        #         if self.utility(board.move(action)) == v]
+
+        """
+            How should we represent state? In order to use the functions in checkerboard.py,
+            we must have a board object. Would it be too much to make a board for each search?    
         """
         raise NotImplemented
 
@@ -36,10 +42,22 @@ class Strategy(abstractstrategy.Strategy):
             else
                 v = -infinity
                 for a in actions(state)
-                    v = max(v, min_val(result(state, a), alpha, beta)
+                    v = max_val(v, min_val(result(state, a), alpha, beta)
                     if v >= beta then break else alpha = max(alpha, v)
             return v
         """
+        # if state.is_terminal():
+        #     return self.utility(state)
+        # else:
+        #     v = -float("inf")
+        #     for a in actions(state):
+        #         v = self.max_val(state, min_val,  alpha, beta)
+        #         if v >= beta:
+        #             break
+        #         else:
+        #             alpha = max(alpha, v)
+        # return v
+
         raise NotImplemented
 
     def min_val(self, state, alpha, beta):
@@ -53,6 +71,25 @@ class Strategy(abstractstrategy.Strategy):
                     if v <= alpha then break else beta = min(beta, v)
             return v
         """
+        raise NotImplemented
+
+
+class Node:
+    """Maybe use nodes?"""
+
+    def __init__(self, state, parent=None, action=None):
+        raise NotImplemented
+
+    def expand(self):
+        raise NotImplemented
+
+    def child_node(self):
+        raise NotImplemented
+
+    def solution(self):
+        raise NotImplemented
+
+    def path(self):
         raise NotImplemented
 
 
