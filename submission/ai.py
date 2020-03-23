1d67cef0e20727bb7377016ee52fa1de3fc6951f
Patrick <perrinepatrick@gmail.com>
2020-03-23 15:54:10 -0700

    compiled submission code, game log, and affidavit into one folder

---
+++ submission/ai.py
@@ -0,0 +1,290 @@
+"""
+Filename: ai.py
+
+Strategy class for Checkers AI, derived from Abstractstrategy
+
+Contains functions for playing checkers, determining utility,
+maximizing utility, and a Node class to help with utility functions.
+
+CS 550, Spring 2020, Marie Roch
+@author: nmill, pperr
+"""
+
+import abstractstrategy
+import checkerboard
+import startlibrary
+
+class Strategy(abstractstrategy.Strategy):
+    """Strategy class definitions"""
+
+    def play(self, board, verbose=False):
+        """
+            Play function - Determines starting move(s), then conducts
+            an alpha-beta search to determine the most optimal move
+        """
+
+        print("%s thinking using ai strategy..." % self.maxplayer)
+        # Get a list of possible actions from maxplayer
+        actions = board.get_actions(self.maxplayer)
+
+        # Initialize starting move library and ai_action variable
+        startlibrary.init_starts()
+        ai_action = None
+
+        # If there are actions to take, then utilize starting move library
+        if actions:
+            # Check to see if the board is at a starting state
+            for start in startlibrary.start:
+                if str(board) == str(start[0]):
+                    for move in start[1]:
+                        if move in actions:
+                            ai_action = move
+
+            # If there are no starting moves to make, then conduct alpha-beta search
+            if ai_action is None:
+                ai_action = self.alpha_beta_search(Node(board), verbose=verbose)
+        else:
+            ai_action = []  # No possible actions
+
+        # If no action was found, then return the board as-is and print error message
+        if not ai_action:
+            newboard = board
+            print("AI not making move.")
+        # If there was a valid action found, execute the move
+        else:
+            newboard = board.move(ai_action)
+
+        # Return updated board and the move that was executed
+        return newboard, ai_action
+
+    def alpha_beta_search(self, node, verbose=False):
+        """
+            Alpha-beta pruning minimax search - Begins by executing the
+            recursive max_val function to derive utility, denoted by v,
+            as well as the most optimal move to make
+
+            Based on the following pseudocode:
+                v = max_val(state, alpha=-infinity, beta=+infinity)
+                return action in actions(state) with value v
+        """
+
+        # Begins minimax pruning and calculates v and the optimal node of search tree
+        v, leafNode = self.max_val(node, alpha=-float("inf"), beta=float("inf"))
+
+        # Prints v value if logging is enabled
+        if verbose:
+            print("v =", v)
+
+        # Returns the move that maximizes the player's utility
+        return leafNode.best_move()
+
+    def max_val(self, node, alpha, beta):
+        """
+            Maximum Value function - Increases the lower bound,
+            alpha, of the max nodes
+
+            Note: node is essentially the state for this implementation,
+            but the state is really the Checkerboard object that is
+            stored within the node, accessed by node.state
+
+            Based on the following pseudocode:
+                if terminal(state) then v = utility(state)
+                else
+                    v = -infinity
+                    for a in actions(state)
+                        v = max(v, min_val(result(state, a), alpha, beta))
+                        if v >= beta then break else alpha = max(alpha, v)
+                return v
+        """
+        leafNode = None
+
+        # If the Checkerboard is at a terminal state, or the depth meets/exceeds
+        # the maxplies value, then return the current utility of the Checkerboard
+        if node.state.is_terminal()[0] or node.depth >= self.maxplies:
+            return self.utility(node.state), node
+
+        # Else, iterate through all possible actions, using min_val to generate leaf nodes
+        # When the utility value meets/exceeds the initial beta value, then break loop
+        else:
+            # Initialize utility to be negative infinity
+            v = -float("inf")
+
+            # Iterates through each action of the Checkerboard, calls min_val to generate nodes
+            for action in node.state.get_actions(self.maxplayer):
+
+                # Creates a new Node to be passed into min_val, assigns current node as new node's parent
+                new_v, leafNode = self.min_val(Node(node.state.move(action), parent=node, action=action),
+                                               alpha, beta)
+
+                # Overrides utility value with the greatest of the old or new value
+                v = max(v, new_v)
+
+                # If the greater utility value surpasses the upper bound, beta, then break loop
+                if v >= beta:
+                    break
+                # Else, override lower bound, alpha, with itself or the utility value (the greater one)
+                else:
+                    alpha = max(alpha, v)
+
+        # Return the greatest possible utility value and the corresponding leaf node that contains the move
+        return v, leafNode
+
+    def min_val(self, node, alpha, beta):
+        """
+            Minimum Value function - Decreases the lower bound,
+            beta, of the min nodes
+
+            Note: node is essentially the state for this implementation,
+            but the state is really the Checkerboard object that is
+            stored within the node, accessed by node.state
+
+            Based on the following pseudocode:
+                if terminal(state) then v = utility(state)
+                else
+                    v = +infinity
+                    for a in actions(state)
+                        v = min(v, max_val(result(state, a), alpha, beta))
+                        if v <= alpha then break else beta = min(beta, v)
+                return v
+        """
+        leafNode = None
+
+        # If the Checkerboard is at a terminal state, or the depth meets/exceeds
+        # the maxplies value, then return the current utility of the Checkerboard
+        if node.state.is_terminal()[0] or node.depth >= self.maxplies:
+            return self.utility(node.state), node
+
+        # Else, iterate through all possible actions, using max_val to generate leaf nodes
+        # When the utility value meets/falls behind the initial alpha value, then break loop
+        else:
+            # Initialize utility to be positive infinity
+            v = float("inf")
+
+            # Iterates through each action of the Checkerboard, calls max_val to generate nodes
+            for action in node.state.get_actions(self.minplayer):
+
+                # Creates a new Node to be passed into max_val, assigns current node as new node's parent
+                new_v, leafNode = self.max_val(Node(node.state.move(action), parent=node, action=action),
+                                               alpha, beta)
+
+                # Overrides utility value with the least of the old or new value
+                v = min(v, new_v)
+
+                # If the lesser utility value meets/falls behind the lower bound, alpha, then break loop
+                if v <= alpha:
+                    break
+
+                # Else, override upper bound, beta, with itself or the utility value (the lesser one)
+                else:
+                    beta = min(beta, v)
+
+        # Return the lowest possible utility value and the corresponding leaf node that contains the move
+        return v, leafNode
+
+    def utility(self, state):
+        """
+            Utility - Contains the logic and rules for the AI
+            to follow to make competitive moves and gain king pieces
+
+            Takes a given Checkerboard, state, and determines the current
+            utility value of the set-up of the current player
+
+            A higher utility value indicates the greater advantage
+            that the current player has
+
+        """
+        # Initialize utility value
+        utility = 0
+
+        # Identify max player (a.k.a current player), minplayer is the competitor
+        pidx = state.playeridx(self.maxplayer)
+
+        # Count the pieces of each type on board for each player
+        numMaxPawns = state.get_pawnsN()[pidx]
+        numMaxKings = state.get_kingsN()[pidx]
+        numMinPawns = state.get_pawnsN()[1 - pidx]
+        numMinKings = state.get_kingsN()[1 - pidx]
+
+        # calculate the distance of each pawn to the king row
+        for i, row in enumerate(state.board):
+            for j, col in enumerate(state.board[i]):
+                if state.board[i][j] and state.board[i][j] != ' ':
+                    piece = state.board[i][j]
+                    playerid, king = state.identifypiece(piece)
+
+                    if playerid == pidx and not king:
+                        utility -= state.disttoking(self.maxplayer, i)
+                    elif playerid == 1 - pidx and not king:
+                        utility += state.disttoking(self.minplayer, i)
+
+                    # add bonus points if the piece is surrounded by same player
+                    # subtract points if the piece is surrounded by opponent
+                    if 1 <= i <= 6 and 1 <= j <= 6:
+                        surround = [(x, y) for x in range(i - 1, i + 1) for y in range(j - 1, j + 1)]
+                        for tile in surround:
+                            if not state.isempty(tile[0], tile[1]):
+                                if state.board[tile[0]][tile[1]] == playerid:
+                                    if playerid == pidx:
+                                        utility += 1
+                                    else:
+                                        utility -= 1
+                                else:
+                                    if playerid == pidx:
+                                        utility -= 1
+                                    else:
+                                        utility += 1
+
+        # Calculate the number of tiles in king row that are exposed to opponent for each player
+        exposedMaxKingTile = sum([1 for action in state.get_actions(self.maxplayer)
+                                  if action[1][1] == 0 or action[1][1] == 7])
+        exposedMinKingTile = sum([1 for action in state.get_actions(self.minplayer)
+                                  if action[1][1] == 0 or action[1][1] == 7])
+
+        # Determines how the center is being controlled, increments utility correspondingly
+        center = ((3, 2), (3, 4), (4, 3), (4, 5))
+        for [row, col] in center:
+            if state.isempty(row, col) is False:
+                piece = state.board[row][col]
+                utility += state.isplayer(self.maxplayer, piece) * 5
+                utility -= state.isplayer(self.minplayer, piece) * 5
+
+        # combine the utilities defined above
+        utility += numMaxPawns * 5 + numMaxKings * 10 - exposedMaxKingTile * 5
+        utility -= numMinPawns * 5 + numMinKings * 10 - exposedMinKingTile * 5
+
+        return int(utility)
+
+
+class Node:
+    """
+        Node class - Contains a Checkerboard as it's state,
+        a Parent Node to build the decision tree, as well as
+        a corresponding action to be taken to reach decision
+    """
+
+    def __init__(self, state, parent=None, action=None):
+        """ Initialize values, assign parent/action, and increment depth if needed """
+
+        self.parent = parent
+        self.state = state
+        if parent:
+            self.depth = parent.depth + 1
+        else:
+            self.depth = 0
+        if action:
+            self.action = action
+        else:
+            self.action = 0
+
+    def best_move(self):
+        """Return the list of actions to reach this node"""
+
+        node, path = self, []
+        # Chase parent pointers, appending each node as it is found
+        while node:
+            path.append(node.action)
+            node = node.parent
+        # List is from goal to initial state, reverse to provide initial state to goal
+        path.reverse()
+
+        return path[1]
---
+++ submission/checkers.py
@@ -0,0 +1,156 @@
+"""
+Filename: checkers.py
+
+Checkers driver class for Checkers AI
+
+Contains the Game function, which operates the game of checkers with
+two AI's or a human versus an AI, depending on the starting variables.
+Also prints the checkers board along with player and turn information.
+
+Contains the main function to run Game.
+
+CS 550, Spring 2020, Marie Roch
+@author: mroch, nmill, pperr
+"""
+
+# Game representation and mechanics
+import checkerboard
+
+# tonto - Professor Roch's not too smart strategy
+# You are not given source code to this, but compiled .pyc files
+# are available for Python 3.7 and 3.8 (fails otherwise).
+# This will let you test some of your game logic without having to worry
+# about whether or not your AI is working and let you pit your player
+# against another computer player.
+#
+# Decompilation is cheating, don't do it.  Big sister is watching you :-)
+
+# Python can load compiled modules using the imp module (deprecated)
+# We'll format the path to the tonto module based on the
+# release of Python.  Note that we provided tonto compilations for Python 3.7
+# and 3.8.  If you're not using one of these, it won't work.
+import imp
+import sys
+
+major = sys.version_info[0]
+minor = sys.version_info[1]
+modpath = "__pycache__/tonto.cpython-{}{}.pyc".format(major, minor)
+tonto = imp.load_compiled("tonto", modpath)
+
+# human - human player, prompts for input
+import human
+import ai
+import boardlibrary  # might be useful for debugging
+from statistics import mean
+from timer import Timer
+
+
+def Game(red=ai.Strategy, black=tonto.Strategy,
+         init=None, maxplies=10, verbose=True, firstmove=0):
+    """
+    Game(red, black, maxplies, init, verbose, turn)
+        Start a game of checkers
+        red,black - Strategy classes (not instances)
+        maxplies - # of turns to explore (default 10)
+        init - Start with given board (default None uses a brand new game)
+        verbose - Show messages (default True)
+        firstmove - Player N starts 0 (red) or 1 (black).  Default 0. 
+    """
+
+    # Print the current board, action taken, piece count, and number of moves
+    def printMove():
+        if len(action[1]) == 3:
+            capture = "Capturing" + str(action[1][1])
+            print("Move %s by %s: from %s to %s capturing %s  Result:" %
+                  (game_board.movecount, players[turn], action[0],
+                   (action[1][0], action[1][1]), action[1][2]))
+        else:
+            print("Move %s by %s: from %s to %s  Result:" %
+                  (game_board.movecount, players[turn], action[0], action[1]))
+        print(game_board)
+        print("Pawn/King count: r %d R %d b %d B %d  Time - move: %d s, game %.1f min" %
+              (game_board.get_pawnsN()[0], game_board.get_kingsN()[0],
+               game_board.get_pawnsN()[1], game_board.get_kingsN()[1],
+               t_move.elapsed_s(), t_game.elapsed_min()))
+        print("Moves since last capture %d last pawn advance %d" %
+              (game_board.lastcapture, game_board.lastpawnadvance))
+        print()
+
+    # Inialize board library and timer
+    boardlibrary.init_boards()
+    t_game = Timer()
+
+    if init:
+        game_board = init
+    else:
+        game_board = checkerboard.CheckerBoard()
+
+    # Initialize players and maxplies values
+    red = red('r', game_board, maxplies)
+    black = black('b', game_board, maxplies)
+
+    # Initialize play lists and print start statement
+    print("How about a nice game of checkers?")
+    turn = firstmove
+    players = ['r', 'b']
+    board_states = []
+    r_moves = []
+    b_moves = []
+    move_times = [r_moves, b_moves]
+
+    # Has red and black players call their play functions until game is in terminal state
+    while not game_board.is_terminal()[0]:
+
+        # Sets timer to determine how many seconds it takes for a player to make a move
+        t_move = Timer()
+
+        print("Player %s turn" % players[turn])
+
+        # Black player makes a move, then appends move to black moves list
+        if turn:
+            game_board, action = black.play(game_board)
+            b_moves.append(t_move.elapsed_s())
+        
+        # Red player makes a move, then appends move to red moves list
+        else:
+            game_board, action = red.play(game_board)
+            r_moves.append(t_move.elapsed_s())
+
+        # If logging is enabled, then print the move made by the player
+        if verbose:
+            printMove()
+
+        # Determines whether to draw the current board and whether to break
+        n = 0
+        draw = False
+        for state in board_states:
+            if game_board.board == state:
+                n += 1
+            if n >= 3:
+                draw = True
+                break
+        
+        # Append the new board state to the list of previous states
+        board_states.append(game_board.board)
+
+        # Switch turn
+        turn = not turn
+
+    # Prints final board, endgame message, and stats for each player
+    print("Final board")
+    print(game_board)
+    if draw:
+        print("Game is a draw")
+    else:
+        print("The winner is %s!" % players[not turn])
+
+    for i, player in enumerate(players):
+        time = mean(move_times[i])
+        print("%s Average move time: %d:%d:%d" % (player, time / 60 / 60, time / 60, time))
+
+
+if __name__ == "__main__":
+    # Game(init=boardlibrary.boards["multihop"])
+    # Game(init=boardlibrary.boards["StrategyTest1"])
+    # Game(init=boardlibrary.boards["EndGame1"], firstmove = 1)
+    Game(black=ai.Strategy, red=tonto.Strategy, maxplies=10, verbose=True, firstmove=1)
